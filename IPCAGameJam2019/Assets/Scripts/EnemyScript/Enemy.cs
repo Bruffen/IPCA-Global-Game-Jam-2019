8b8e0886f0536164a5dd9f87d8b4e65c754a6590
moises <moises.moreira.bio@gmail.com>
2019-01-26 02:54:05 +0000

    Nesta build knockback feito

---
+++ IPCAGameJam2019/Assets/Scripts/EnemyScript/Enemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    protected int health;
+    protected Transform tPlayer;
+    protected Vector2 velocity;
+
+    protected float knockValue;
+    protected Vector2 knockValeuVelocity;
+
+    protected float farsight;
+    protected Rigidbody2D enRigidBody;
+    
+   
+
+    // Start is called before the first frame update
+   protected virtual void Start()
+    {
+        tPlayer = GameObject.FindGameObjectWithTag("Player").transform;
+        velocity = Vector2.zero;
+        enRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        enRigidBody.position += velocity + knockValeuVelocity;
+        knockValeuVelocity *= 0.9f;
+    }
+
+    protected virtual Vector2 Knockback()
+    {
+        Vector2 finalResult;
+        finalResult = (this.transform.position - tPlayer.position ) * knockValue;
+        return finalResult;
+    }
+
+    protected virtual bool SensePlayer()
+    {
+        if (Vector2.Distance(tPlayer.position, this.transform.position) > farsight) return false;
+        else return true;
+    }
+
+    /***/
+    protected void AiBehaviour()
+    {
+        
+    }
+
+    private void takeDamage()
+    {
+
+    }
+}
---
+++ IPCAGameJam2019/Assets/Scripts/EnemyScript/MeleeEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemy : Enemy
+{
+    
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        farsight = 2;
+        knockValue = 0.5f;
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.transform == tPlayer)
+        {
+            knockValeuVelocity += Knockback();
+
+        }
+
+    }
+
+
+}
